8be32b0ee7b54431c818ebea9cb416818b1509da
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-18 14:11:02 +0530

    Performance improvements

--- ds3231-oled-buzzer-clock/libs/particles.h
+++ ds3231-oled-buzzer-clock/libs/particles.h
@@ -1,33 +1,37 @@
 #ifndef PARTICLES
-#define PARTICLES 20
+#define PARTICLES 30
 #endif
 
 // Paint the particles
-short particles[PARTICLES][2] ;
+unsigned char particles[PARTICLES][4] ;
 
 void initParticles() {
-	static short *p ;
+	static unsigned char *p ;
 
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
-		p[0] = rand() % 128 ;
-		p[1] = rand() % 64 ;
+		p[0] = rand() & 127 ;
+		p[1] = rand() & 63 ;
+		p[2] = p[0] & 1 ? 1 : -1 ;
+		p[3] = p[1] & 1 ? 1 : -1 ;
 	}
 }
 
 void drawParticles() {
-	static short *p ;
+	static unsigned char *p ;
 
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
-		p[0] += round(sin(i)) ;
-		p[1] += round(cos(i)) ;
-
-		if (p[1] > 64 || p[0] > 128 || p[1] < 1 || p[0] < 1) {
-			p[0] = rand() % 128 ;
-			p[1] = rand() % 64 ;
+		if ((p[0] & 127) && (p[1] & 63)) {
+			p[0] += p[2] ;
+			p[1] += p[3] ;
+		} else {
+			p[0] = rand() & 127 ;
+			p[1] = rand() & 63 ;
+			p[2] = p[0] & 1 ? 1 : -1 ;
+			p[3] = p[1] & 1 ? 1 : -1 ;
 		}
 
 		display.drawPixel(p[0], p[1], WHITE) ;
