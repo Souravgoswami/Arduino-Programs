b36eab2fb58d2884f84588c9df72beb50b29af65
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-18 14:11:21 +0530

    Performance improvements

--- ds3231-oled-buzzer-clock/ds3231-oled-buzzer-clock.ino
+++ ds3231-oled-buzzer-clock/ds3231-oled-buzzer-clock.ino
@@ -1,13 +1,13 @@
 #pragma GCC optimize("O3")
 
+// Max usable global variable is ~850 bytes
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define SCREEN_ADDRESS 0x3C
 #define DISPLAYROTATION 0
 #define TEXTWRAP false
-#define BAUDRATE 115200
 #define SPLITTER_DELAY 250
-#define PARTICLES 55
+#define PARTICLES 75
 
 #define BUZZER 11
 #define BUZZ_DELAY 50
@@ -42,6 +42,9 @@ const char *const dayTable[] PROGMEM = {
 } ;
 
 const unsigned short buzzDelay4 = BUZZ_DELAY * 4 ;
+const unsigned short buzzDelay3 = BUZZ_DELAY * 3 ;
+const unsigned short buzzDelay2 = BUZZ_DELAY * 2 ;
+unsigned char doubleBuzz = 0 ;
 
 unsigned char jiffies = 0 ;
 unsigned long pastJiffies = 0 ;
@@ -52,11 +55,11 @@ char splitter = ':' ;
 unsigned long splitterChangeDelay = millis() + SPLITTER_DELAY ;
 static unsigned long milliSec ;
 
-static unsigned char hour, minute, second, pastSecond ;
-unsigned char buzz = 0 ;
+static unsigned char hour, lastHour, minute, pastMinute, second, pastSecond ;
+static unsigned char day, month ;
+static unsigned short year ;
+
 unsigned long buzzed = 0 ;
-unsigned char lastHour = 0 ;
-unsigned char doubleBuzz = 0 ;
 
 static unsigned char weekDay ;
 char dayOfWeek[4] ;
@@ -77,7 +80,7 @@ void setup () {
 		display.setCursor(20, 32) ;
 		display.print("No RTC!") ;
 		display.display() ;
-		
+
 		while(1) ;
 	}
 
@@ -97,7 +100,19 @@ void setup () {
 	digitalWrite(BUZZER, LOW) ;
 
 	DateTime now = rtc.now() ;
-	lastHour = now.hour() ;
+	second = now.second() ;
+
+	// Always differ pastSecond at the start of program to
+	// initiate other second based features like minute.
+	pastSecond = second + 1 ;
+
+	// Always differ pastMinute at the start of program to
+	// initiate other minute based features like hour.
+	pastMinute = minute + 1 ;
+
+	// Always differ lastHour at the start of program to
+	// initiate other hour based features like day, month and year.
+	lastHour = hour + 1 ;
 	weekDay = now.dayOfTheWeek();
 
 	strcpy_P(
@@ -108,52 +123,56 @@ void setup () {
 
 void loop () {
 	display.clearDisplay() ;
+	drawParticles() ;
+
 	milliSec = millis() ;
 
 	DateTime now = rtc.now() ;
 	second = now.second() ;
-	hour = now.hour() ;
-	minute = now.minute() ;
+
+	if(second != pastSecond) {
+		pastSecond = second ;
+		pastJiffies = milliSec ;
+
+		splitterChangeDelay = milliSec + SPLITTER_DELAY ;
+		splitter = ':' ;
+
+		minute = now.minute() ;
+	}
+
+	if (minute != pastMinute) {
+		pastMinute = minute ;
+		hour = now.hour() ;
+	}
 
 	if (hour != lastHour) {
-		buzz = 1 ;
 		digitalWrite(BUZZER, HIGH) ;
 		buzzed = milliSec + BUZZ_DELAY ;
 		lastHour = hour ;
-		
+
+		doubleBuzz = (hour == 0 || hour == 12) ? 1 : 0 ;
+
 		weekDay = now.dayOfTheWeek() ;
 		strcpy_P(
 			dayOfWeek,
 			(char *)pgm_read_word(&(dayTable[weekDay]))
 		) ;
+
+		day = now.day() ;
+		month = now.month() ;
+		year = now.year() ;
 	}
 
-	doubleBuzz = (hour == 0 || hour == 12) ? 1 : 0 ;
+	if (milliSec > buzzed) {
+		digitalWrite(BUZZER, LOW) ;
+	}
 
 	if (doubleBuzz) {
-		if (milliSec > buzzed + buzzDelay4) {
-			digitalWrite(BUZZER, LOW) ;
-		} else if (milliSec > buzzed + BUZZ_DELAY * 3) {
-			digitalWrite(BUZZER, HIGH) ;
-		} else if (milliSec > buzzed + BUZZ_DELAY * 2) {
+		if (milliSec > buzzed + buzzDelay2) {
 			digitalWrite(BUZZER, LOW) ;
 		} else if (milliSec > buzzed + BUZZ_DELAY) {
 			digitalWrite(BUZZER, HIGH) ;
-		} else if (milliSec > buzzed) {
-			digitalWrite(BUZZER, LOW) ;
 		}
-	} else {
-		if (milliSec > buzzed) {
-			digitalWrite(BUZZER, LOW) ;
-		}
-	}
-
-	if(second != pastSecond) {
-		pastSecond = second ;
-		pastJiffies = milliSec ;	
-
-		splitterChangeDelay = milliSec + SPLITTER_DELAY ;
-		splitter = ':' ;
 	}
 
 	jiffies = (milliSec - pastJiffies) / 10 ;
@@ -165,8 +184,6 @@ void loop () {
 		splitter = ' ' ;
 	}
 
-	drawParticles() ;
-
 	// Show day of the week
 	display.setFont(&BIG_FONT) ;
 	display.setCursor(44, 14) ;
@@ -175,10 +192,10 @@ void loop () {
 	// Show date
 	sprintf(
 		tempStr,
-		"%02d/%02d/%02d",
-		now.day(), now.month(), now.year()
+		"%02d/%02d/%04d",
+		day, month, year
 	) ;
-	
+
 	display.setFont(&MEDIUM_FONT) ;
 	display.setCursor(2, 34) ;
 	display.print(tempStr) ;
